1f3f4d3beda8b5fdb24860820e772c2533f13b83
Michael Thomsen <mit@google.com>
2020-12-03 12:01:17 +0100

    Use github predefined error output format

--- setup.sh
+++ setup.sh
@@ -16,11 +16,6 @@ ARCH="${3:-x64}"
 OS=$(echo "$OS" | awk '{print tolower($0)}')
 echo "Installing Dart SDK from the ${CHANNEL} channel on ${OS}-${ARCH}"
 
-# Define colors.
-WHITE='\033[0m'
-GREEN='\033[32m'
-RED='\033[31m'
-
 # Calculate download Url. Based on:
 # https://dart.dev/tools/sdk/archive#download-urls
 PREFIX="https://storage.googleapis.com/dart-archive/channels"
@@ -31,7 +26,7 @@ echo "Downloading ${URL}..."
 curl --connect-timeout 15 --retry 5 "$URL" > "${HOME}/dartsdk.zip"
 unzip "${HOME}/dartsdk.zip" -d "${RUNNER_TOOL_CACHE}" > /dev/null
 if [ $? -ne 0 ]; then
-  echo -e "${RED}ERROR: Download failed! Please check passed arguments.${WHITE}"
+  echo -e "::error::Download failed! Please check passed arguments."
   exit 1
 fi
 rm "${HOME}/dartsdk.zip"
@@ -41,4 +36,4 @@ echo "${HOME}/.pub-cache/bin" >> $GITHUB_PATH
 echo "${RUNNER_TOOL_CACHE}/dart-sdk/bin" >> $GITHUB_PATH
 
 # Report success.
-echo -e "${GREEN}Succesfully installed Dart SDK.${WHITE}"
+echo -e "Succesfully installed Dart SDK."
